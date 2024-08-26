617015582b6f4929a2ad163836bd617f895696d0
Marjanno Evert <marjanno.evert@tktk.ee>
2024-08-26 22:00:30 +0300

    add week2 improvement

--- UserAgeValidation/Program.cs
+++ UserAgeValidation/Program.cs
@@ -8,13 +8,26 @@
 
 Console.WriteLine("Enter your age:");
 
-int userAge = Int32.Parse(Console.ReadLine()); // "13" - heap, 13 - stack
+string? userAge = Console.ReadLine();
 
-if (userAge >= 13)
+int userAgeNum;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber)
 {
-    Console.WriteLine("Welcome to Instagram!");
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
 }
-else //if (userAge < 13)
+else
 {
-    Console.WriteLine("You are too young to use Instagram");
+    Console.WriteLine("Could not read your age.");
 }
